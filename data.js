641b16d7511e3bf79630933e363ed49d0ae2ca1e
suriyabroop <98744069+suriyabroop@users.noreply.github.com>
2022-03-08 20:32:24 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+mongoLink:'mongodb+srv://tnvillanyt:suriyaskp12@cluster0.tzm8c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',// Database Link Of MongoDB
+
+apikey:'EamGXayT3yLF0tUK',// Coinbase API Key
+
+secretkey:'QIyYjkevBj4L0WIxB3Ha252kUkYbUG6l ',// Coinbase 2nd Key
+
+bot_token:'LTC',// Bot Token
+
+bot_name:'LITECOIN_PRO_BOT',// Bot Username Without @
+
+bot_admin:1996486622,// Admin Telegram Id
+
+channelsList : ['@payout_litecoin'],// Channels List
+
+channelscheck : ['@payout_litecoin'],// Check Channels List
+
+reffer_bonus:0.00001,// Refferal Bonus Amount
+
+min_wd:0.00001,// Min Withdrawal Amount
+
+daily_bonus:0.000001,// Daily Bonus Amount
+
+currency:'LTC', // Bot Currency
+
+payment_channel:'@payout_litecoin'// Payment Channel Username
+
+}
